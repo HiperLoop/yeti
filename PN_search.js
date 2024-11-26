b318dfcdc4ee8e679fe5422714fafd8267aed85a
HiperLoop <121406263+HiperLoop@users.noreply.github.com>
2024-11-26 12:27:24 +0100

    started working on PN search

---
+++ PN_search.js
@@ -0,0 +1,8 @@
+const {winLength, gridSize, board, checkWin} = require('./game.js');
+const {pn_tree, node} = require('./PN_tree.js');
+
+function searchNode(origin) {
+    
+}
+
+module.exports = {searchNode};
